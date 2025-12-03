b3404789714e9eba2aef03d518d17863f31c18d8
Théo Bori <theobori@disroot.org>
2025-12-03 01:24:18 +0100

    :sparkles: 2025 day 1

---
+++ 2025/01/part2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env pypy
+
+import os
+
+with os.fdopen(0) as f:
+    lines = [(x[0], int(x[1:])) for x in f.read().splitlines() if len(x) > 0]
+
+dial = 50
+next_dial = ans = 0
+
+for direction, distance in lines:
+    if direction == "L":
+        delta = -1
+    elif direction == "R":
+        delta = 1
+    else:
+        raise Exception("Unknown direction")
+
+    for _ in range(distance):
+        dial += delta
+
+        if dial == 0:
+            ans += 1
+        elif dial < 0:
+            dial = 99
+        elif dial >= 100:
+            dial = 0
+            ans += 1
+
+
+print(ans)
