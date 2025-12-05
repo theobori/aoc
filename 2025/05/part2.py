535bbdcc94bb4b93431f497f7006ec59fc3ef756
Théo Bori <theobori@disroot.org>
2025-12-05 14:43:42 +0100

    :sparkles: 2025 day 5

---
+++ 2025/05/part2.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env pypy
+
+import os
+
+from collections import deque
+
+with os.fdopen(0) as f:
+    ranges_str, ids_str = f.read().strip().split("\n\n")
+
+ranges = []
+for range_str in ranges_str.splitlines():
+    pair = list(map(int, range_str.split("-")))
+
+    ranges.append(pair)
+
+ans = 0
+
+n = len(ranges)
+ranges = deque(ranges)
+
+for _ in range(n):
+    start, end = ranges.popleft()
+
+    for i, (other_start, other_end) in enumerate(ranges):
+        if other_start <= start <= other_end and end >= other_end:
+            ranges[i][1] = end
+            break
+        elif other_start <= end <= other_end and start <= other_start:
+            ranges[i][0] = start
+            break
+        elif other_start <= start <= end <= other_end:
+            break
+        elif start <= other_start and end >= other_end:
+            ranges[i][0] = start
+            ranges[i][1] = end
+            break
+    else:
+        ranges.append([start, end])
+
+ans = 0
+for start, end in ranges:
+    ans += end - start + 1
+
+print(ans)
